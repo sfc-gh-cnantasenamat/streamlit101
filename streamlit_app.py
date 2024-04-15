df206a65a88ba5ea2757c01d810fc708042f20aa
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-15 10:20:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ year_list = list(df.year.unique())[::-1]
 # selected_year = st.selectbox('Select a year', year_list)
 selected_year = st.number_input('Enter a year',
                                  placeholder='Enter a  year from 2010-2019',
-                                 format='str')
+                                 format=str)
 
 # Data processing
 if selected_year:
