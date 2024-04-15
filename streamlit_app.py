9e5bdaf9e9f86b3e4670ee4d4851912c5097a1af
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-15 10:14:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ year_list = list(df.year.unique())[::-1]
 selected_year = st.text_input('Enter a year')
 
 # Data processing
-df_selected_year = df[df.year == selected_year]
+df_selected_year = df[df.year == int(selected_year)]
 df_selected_year
