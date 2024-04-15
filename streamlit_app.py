f9d485c1806c4fd73839ed0f78e9425b46de5af9
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-15 09:45:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ import pandas as pd
 # st.title('🎈 My First App')
 
 # Load CSV data
-df = pd.read_csv('data/us-population-2010-2019.csv', index_col=False)
+df = pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
 df
