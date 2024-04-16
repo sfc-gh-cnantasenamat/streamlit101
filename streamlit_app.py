4c276c7db10054875b1cf1ac498320df58e9cc8e
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-15 18:06:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,14 @@ st.title('🎈 My First Streamlit App')
 # Load CSV data
 df = pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
 
-# Year selector
-year_list = list(df.year.unique())[::-1]
+# Year selectbox
+# selected_year = st.selectbox('Select a year', list(df.year.unique())[::-1])
 
-# selected_year = st.selectbox('Select a year', year_list)
+# Year number_input
 selected_year = st.number_input('Enter a year',
                                  placeholder='Enter a  year from 2010-2019',
                                  value=2019)
 
-# Data processing
+# Display data subset
 df_selected_year = df[df.year == selected_year]
 df_selected_year
