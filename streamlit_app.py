a58a2f309815689b49544ba4e43ff43790e12721
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-17 00:16:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,6 @@ selected_year = st.number_input('Enter a year',
 
 # Display data subset
 df_selected_year = df[df.year == selected_year]
-st.dataframe(df_selected_year, height=200)
+st.dataframe(df_selected_year, height=250)
 
 st.area_chart(df_selected_year, x='states', y='population')
