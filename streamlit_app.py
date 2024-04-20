60c8a81c1cb700cd3e1fccc2b6958129bf9f2960
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-19 20:45:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 # Import libraries
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import time
+#import plotly.express as px
 
-# st.write('Hello world!')
+#st.write('Hello world!')
 
 # App title
 st.title('🎈 My First Streamlit App')
@@ -32,6 +32,7 @@ df = pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
 
 #st.write('Using st.cache_data', (t1-t0)*1000, 'milliseconds')
 
+
 # Year selection
 # Using st.selectbox
 selected_year = st.selectbox('Select a year', list(df.year.unique())[::-1])
@@ -41,9 +42,11 @@ selected_year = st.selectbox('Select a year', list(df.year.unique())[::-1])
 #                                 placeholder='Enter a  year from 2010-2019',
 #                                 value=2019)
 
+
 # Display data subset
 df_selected_year = df[df.year == selected_year]
 st.dataframe(df_selected_year, height=250, use_container_width=True)
+
 
 # Display chart
 st.area_chart(df_selected_year, x='states', y='population')
