2f289b2d7462be41cbb9ff651d3d7b0a4ecca343
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-19 20:41:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,11 @@ x1 = time.time()
 st.write('Not using st.cache_data', (x1-x0)*1000, 'milliseconds')
 
 # Caching data
+t0 = time.time()
 @st.cache_data
 def load_data():
   return pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
 
-t0 = time.time()
 df = load_data()
 t1 = time.time()
 
