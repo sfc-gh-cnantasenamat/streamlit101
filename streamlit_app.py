7cf0a990db90799bc677bbc4789a84c2248986ad
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-19 20:44:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,28 +11,32 @@ st.title('🎈 My First Streamlit App')
 
 # Load CSV data
 
-# No caching of data
-x0 = time.time()
 df = pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
-x1 = time.time()
+
+# Caching data
+# Not using st.cache_data
+#x0 = time.time()
+#df = pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
+#x1 = time.time()
 
 st.write('Not using st.cache_data', (x1-x0)*1000, 'milliseconds')
 
-# Caching data
-t0 = time.time()
-@st.cache_data
-def load_data():
-  return pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
+# Using st.cache_data
+#t0 = time.time()
+#@st.cache_data
+#def load_data():
+#  return pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
 
-df = load_data()
-t1 = time.time()
+#df = load_data()
+#t1 = time.time()
 
-st.write('Using st.cache_data', (t1-t0)*1000, 'milliseconds')
+#st.write('Using st.cache_data', (t1-t0)*1000, 'milliseconds')
 
-# Year selectbox
+# Year selection
+# Using st.selectbox
 selected_year = st.selectbox('Select a year', list(df.year.unique())[::-1])
 
-# Year number_input
+# Using st.number_input
 #selected_year = st.number_input('Enter a year',
 #                                 placeholder='Enter a  year from 2010-2019',
 #                                 value=2019)
@@ -41,4 +45,5 @@ selected_year = st.selectbox('Select a year', list(df.year.unique())[::-1])
 df_selected_year = df[df.year == selected_year]
 st.dataframe(df_selected_year, height=250, use_container_width=True)
 
+# Display chart
 st.area_chart(df_selected_year, x='states', y='population')
