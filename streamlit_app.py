6fd47146d3d2bb0539aee360dad02f54a143e1e4
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-27 00:05:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,55 +1,51 @@
-# Import libraries
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 import time
 
-#st.write('Hello world!')
+st.header('My App')
 
-# App title
-st.title('🎈 My First Streamlit App')
+# Load data
+#df = pd.read_csv('https://github.com/dataprofessor/population-dashboard/raw/master/data/us-population-2010-2019-reshaped.csv', index_col=0)
+#df
 
-# Load CSV data
+# Caching data experiment
 
-#df = pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
-
-
-# Caching data
 # Not using st.cache_data
-x0 = time.time()
-df = pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
-x1 = time.time()
-
+#x0 = time.time()
+#df1 = pd.read_csv('https://github.com/dataprofessor/population-dashboard/raw/master/data/us-population-2010-2019-reshaped.csv', index_col=0)
+#x1 = time.time()
 
 # Using st.cache_data
-t0 = time.time()
+#t0 = time.time()
 @st.cache_data
 def load_data():
-  return pd.read_csv('data/us-population-2010-2019.csv', index_col=0)
+    return pd.read_csv('https://github.com/dataprofessor/population-dashboard/raw/master/data/us-population-2010-2019-reshaped.csv', index_col=0)
+
+# df2 = load_data()
+#t1 = time.time()
+
+# Benchmark results
+#with st.expander('Benchmark results'):
+#    st.write('Not using st.cache_data', round((x1-x0)*1000, 3), 'milliseconds')
+#    st.write('Using st.cache_data', round((t1-t0)*1000, 3), 'milliseconds')
 
 df = load_data()
-t1 = time.time()
-
-# Results displayed at the bottom
 
 # Year selection
-# Using st.selectbox
-selected_year = st.selectbox('Select a year', list(df.year.unique())[::-1])
-
 # Using st.number_input
 #selected_year = st.number_input('Enter a year',
-#                                 placeholder='Enter a  year from 2010-2019',
-#                                 value=2019)
+#                                placeholder='Enter a year from 2010-2019',
+#                                value=2019)
 
+# Using st.selectbox
+selected_year = st.selectbox('Select a year',
+                             list(df.year.unique())[::-1])
 
 # Display data subset
 df_selected_year = df[df.year == selected_year]
 st.dataframe(df_selected_year, height=250, use_container_width=True)
 
-
 # Display chart
-st.bar_chart(df_selected_year, x='states', y='population')
-
-
-#with st.expander('Benchmarks'):
-#  st.write('Not using st.cache_data', round((x1-x0)*1000, 3), 'milliseconds')
-#  st.write('Using st.cache_data', round((t1-t0)*1000, 3), 'milliseconds')
+st.bar_chart(df_selected_year,
+             x='states',
+             y='population')
